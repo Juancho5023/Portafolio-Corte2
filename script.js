f42e4116286f6a25b4fe85e12305e3a2fab6ce5b
juanchocrack 32 <jgomezr@gmail.com>
2024-09-29 18:53:07 -0500

    portafolio1

---
+++ script.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function moveSlide(direction) {
+    const totalSlides = slides.length;
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    const sliderContainer = document.querySelector('.slides');
+    sliderContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Smooth scroll en todas las anclas
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
